c9c6a1231c909d95e3abe7edf961273d0a68027a
Supitha <supithaweerasinghe@Supithas-MacBook-Pro.local>
2017-06-02 17:15:03 +0530

    adding comments

--- PokerServer/src/pokerserver/PokerServer.java
+++ PokerServer/src/pokerserver/PokerServer.java
@@ -16,9 +16,12 @@ public class PokerServer {
     
     void creatingCards(){
         
+        //creating an array list
         ArrayList cards = new ArrayList();
         
         for(int i=0;i<4;i++){
+            //there are 4 types of cards spades,clubs,diamods,hearts
+            //adding 14 cards belongs to spade
             if(i==0){
                 String type = "spade";
                 for(int x=2;x<=14;x++){
@@ -27,6 +30,7 @@ public class PokerServer {
                     a.ctype = type;
                     cards.add(a);
                 }
+            //adding 14 cards belongs to clubs
             }else if(i==1){
                 String type = "clubs";
                 for(int x=2;x<=14;x++){
@@ -35,6 +39,7 @@ public class PokerServer {
                     a.ctype = type;
                     cards.add(a);
                 }
+            //adding 14 cards belongs to diamonds
             }else if(i==2){
                 String type = "diamonds";
                 for(int x=2;x<=14;x++){
@@ -43,6 +48,7 @@ public class PokerServer {
                     a.ctype = type;
                     cards.add(a);
                 }
+            //adding 14 cards belongs to hearts
             }else{
                 String type = "hearts";
                 for(int x=2;x<=14;x++){
