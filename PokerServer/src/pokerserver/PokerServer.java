9029af379ced89fae30313596cbf6d77a414138b
Supitha <supithaweerasinghe@Supithas-MacBook-Pro.local>
2017-06-03 13:54:45 +0530

    add class CardLogic

--- PokerServer/src/pokerserver/PokerServer.java
+++ PokerServer/src/pokerserver/PokerServer.java
@@ -10,90 +10,15 @@ package pokerserver;
  * @author supithaweerasinghe
  */
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-
 public class PokerServer {
     
-    //creating an array list
-    ArrayList cards = new ArrayList();
-    
-    void creatingCards(){
-        
-        for(int i=0;i<4;i++){
-            //there are 4 types of cards spades,clubs,diamods,hearts
-            //adding 14 cards belongs to spade
-            if(i==0){
-                String type = "spade";
-                for(int x=2;x<=14;x++){
-                    Cards a = new Cards();
-                    a.cnumber = x;
-                    a.ctype = type;
-                    cards.add(a);
-                }
-            //adding 14 cards belongs to clubs
-            }else if(i==1){
-                String type = "clubs";
-                for(int x=2;x<=14;x++){
-                    Cards a = new Cards();
-                    a.cnumber = x;
-                    a.ctype = type;
-                    cards.add(a);
-                }
-            //adding 14 cards belongs to diamonds
-            }else if(i==2){
-                String type = "diamonds";
-                for(int x=2;x<=14;x++){
-                    Cards a = new Cards();
-                    a.cnumber = x;
-                    a.ctype = type;
-                    cards.add(a);
-                }
-            //adding 14 cards belongs to hearts
-            }else{
-                String type = "hearts";
-                for(int x=2;x<=14;x++){
-                    Cards a = new Cards();
-                    a.cnumber = x;
-                    a.ctype = type;
-                    cards.add(a);
-                }
-            }
-        }
-        
-        //printing all the cards
-        /*for(int i=0;i<52;i++){
-            Cards a = (Cards)cards.get(i);
-            System.out.print(a.cnumber);
-            System.out.println(a.ctype);
-        }*/
-        
-    }
-    
-    void randomizingCards(){
-        //randomizing arraylist - cards / shuffling
-        long seed = System.nanoTime();
-        Collections.shuffle(cards,new Random(seed));
-        
-        //printing all the randomized cards
-        ////
-        for(int i=0;i<52;i++){
-            Cards a = (Cards)cards.get(i);
-            System.out.print(a.cnumber);
-            System.out.println(a.ctype);
-        }
-        ////
-    }
-    
- 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        PokerServer p = new PokerServer();
-        p.creatingCards();
-        p.randomizingCards();
+        CardLogic l = new CardLogic();
+        l.creatingCards();
+        l.randomizingCards();
     }
     
 }
